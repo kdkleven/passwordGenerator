20421172baa689e017761f19bdcc51f320b8485e
SageTengu <kyle.kleven@outlook.com>
2021-01-03 19:21:55 -0600

    Updated password length function

--- script/script.js
+++ script/script.js
@@ -20,44 +20,56 @@
 
 var generateBtn = document.querySelector("#generate");
 
+var alphabet = "abcdefghijklmnopqrstuvqxyz";
+var numeric = '0123456789';
+var specChar = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+var character = "";
 
-//Array of letters for lowercase and uppercase (with toUpper) criteria
-var arrAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//Array of numbers for numeric criteria
-var arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-//Array of special characters for spec.char. criteria
-var arrSpecChar = [
-  "+", "-", "&&", "||", "!", "(",
-  ")", "{", "}", "[", "]", "^",
-  "~", "*", "?", ":", "\"", "\\"];
+var pwdProfile = {
+  length: 0,
+  upperCase: true,
+  lowerCase: false,
+  numbers: true,
+  specChar: true,
+  userPassword: "",
 
-  var pwdProfile = {
-    "length": 0,
-    "upperCase": true,
-    "lowerCase": false,
-    "numbers": true,
-    "specChar": true,
-
-    pwdLength: function () {
-      this.length = parseInt(prompt("Choose your password length (must be between 8 and 128 characters"));
-    },
-
-    pwdUpperCase: function () {
-      this.upperCase = confirm("Do you want to include upper case letters?");
-    },
-
-    pwdLowerCase: function () {
-      this.lowerCase = confirm("Do you want to include lower case letters?");
-    },
-
-    pwdNumbers: function () {
-      this.numbers = confirm("Do you want to include numbers?");
-    },
-
-    pwdSpecChar: function () {
-      this.specChar = confirm("Do you want to include special characters?");
+  pwdLength: function (newLength) {
+    newLength = parseInt(prompt("Choose your password length (must be between 8 and 128 characters"));
+    
+    while (newLength < 8 || newLength > 128) {
+      newLength = parseInt(prompt("Choose your password length (must be between 8 and 128 characters"));
     }
-  };
+    alert("You chose a password length of " + newLength + " characters.");  
+    this.length = newLength;
+    //console.log(this.length);
+    return(newLength);
+  },
+
+  pwdUpperCase: function () {
+    this.upperCase = confirm("Do you want to include upper case letters?");
+
+  },
+
+  pwdLowerCase: function () {
+    this.lowerCase = confirm("Do you want to include lower case letters?");
+  },
+
+  pwdNumbers: function () {
+    this.numbers = confirm("Do you want to include numbers?");
+  },
+
+  pwdSpecChar: function () {
+    this.specChar = confirm("Do you want to include special characters?");
+  }
+};
+
+function pwdCompiler() {
+  if (pwdProfile.upperCase === true) {
+    
+  }
+}
+
+//userPassword = Math.floor(Math.random() * pwdProfile.length)
 
 function reWriteProfile() {
   console.log("length: " + pwdProfile.length);
@@ -75,7 +87,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-  
+
 
 
 }
@@ -90,7 +102,7 @@ function generatePassword() {
   pwdProfile.pwdUpperCase();
   pwdProfile.pwdLowerCase();
   pwdProfile.pwdNumbers();
-  pwdProfile.pwdSpecChar();  
+  pwdProfile.pwdSpecChar();
   reWriteProfile();
 };
 
