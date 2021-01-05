29d0c3296d9a62ab22fdbe82aaab90f1b0bde2b2
Kyle Kleven <kylekleven8@gmail.com>
2021-01-05 00:22:56 -0600

    Refactored variables and adjusted generatePassword structure

--- script/script.js
+++ script/script.js
@@ -20,32 +20,31 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var string = "abcdefghijklmnopqrstuvqxyz";
-var numeric = '0123456789';
+var letters = 'abcdefghijklmnopqrstuvqxyz';
+var numbers = '0123456789';
 var symbols = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-var compiled = "";
 
-var pwdProfile = {
+var profile = {
   length: 0,
   upperCase: true,
   lowerCase: true,
-  numbers: true,
+  numeric: true,
   specChar: true,
 
   pwdLength: function (newLength) {
     newLength = parseInt(prompt("Choose your password length (must be between 8 and 128 characters"));
-    
+
     while (newLength < 8 || newLength > 128) {
       newLength = parseInt(prompt("Choose your password length (must be between 8 and 128 characters"));
     }
     this.length = newLength;
     console.log("Length = " + this.length);
-    return(newLength);
+    return (newLength);
   },
 
   pwdUpperCase: function () {
     this.upperCase = confirm("Do you want to include upper case letters?");
-      console.log("UpperCase = " + this.upperCase);
+    console.log("UpperCase = " + this.upperCase);
   },
 
   pwdLowerCase: function () {
@@ -53,24 +52,24 @@ var pwdProfile = {
     console.log("LowerCase = " + this.lowerCase);
   },
 
-  pwdNumbers: function () {
-    this.numbers = confirm("Do you want to include numbers?");
-      console.log("Numeric = " + this.numbers);
+  pwdNumeric: function () {
+    this.numeric = confirm("Do you want to include numbers?");
+    console.log("Numeric = " + this.numeric);
   },
 
   pwdSpecChar: function () {
     this.specChar = confirm("Do you want to include special characters?");
-      console.log("SpecChar = " + this.specChar);
+    console.log("SpecChar = " + this.specChar);
   }
 };
-        
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   console.log("This is the password value: " + password);
   passwordText.value = password;
-  
+
   // password1 = Math.ceil(toUpper(alphabet.length) * Math.random()*Math.random());
   // password2 = Math.ceil(alphabet.length * Math.random()*Math.random());
   // password3 = Math.ceil(numeric.length * Math.random()*Math.random());
@@ -81,52 +80,49 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  pwdProfile.pwdLength();
-  pwdProfile.pwdUpperCase();
-  pwdProfile.pwdLowerCase();
-  pwdProfile.pwdNumbers();
-  pwdProfile.pwdSpecChar();
- 
-    if (pwdProfile.upperCase === true) {
-      var upper = string.toUpperCase();
-      compiled += upper;
-      console.log("Upper Case Result = " + compiled);
-      console.log(compiled.length);
-    }
-    
-    if (pwdProfile.lowerCase === true) {
-      compiled += string;
-      console.log("Lower case Result = " + compiled);
-      console.log(compiled.length);
-    }
-    
-    if (pwdProfile.numbers === true) {
-      compiled += numeric;
-      console.log("Numeric Result = " + compiled);
-      console.log(compiled.length);
-    }
-    
-    if (pwdProfile.specChar === true) {
-      compiled += symbols;
-      console.log("Symbols Result = " + compiled);
-      console.log(compiled.length);
-    }
-    
-    //var compiledLength = compiled.length;
-    var passwordGenerated = "";
-    
-    for (var i = 0; i < pwdProfile.length; i++) {
-      var randomChar = Math.floor(Math.random() * compiled.length);
-      passwordGenerated += compiled.charAt(randomChar); 
-    }
-    console.log(passwordGenerated + " is " + passwordGenerated.length + " characters long");
-    
+  profile.pwdLength();
+  profile.pwdUpperCase();
+  profile.pwdLowerCase();
+  profile.pwdNumeric();
+  profile.pwdSpecChar();
 
-  // console.log("length: " + pwdProfile.length);
-  // console.log("upper case: " + pwdProfile.upperCase);
-  // console.log("lower case: " + pwdProfile.lowerCase);
-  // console.log("numbers: " + pwdProfile.numbers);
-  // console.log("spec char: " + pwdProfile.specChar);
-  // console.log("--------------------------------");
+  var compiled = "";
+  var randomized = "";
+  
+  if (profile.upperCase === true) {
+    var upper = letters.toUpperCase();
+    compiled += upper;
+    console.log("Upper Case Result = " + compiled);
+    console.log(compiled.length);
+  }
+
+  if (profile.lowerCase === true) {
+    compiled += letters;
+    console.log("Lower case Result = " + compiled);
+    console.log(compiled.length);
+  }
+
+  if (profile.numeric === true) {
+    compiled += numbers;
+    console.log("Numeric Result = " + compiled);
+    console.log(compiled.length);
+  }
+
+  if (profile.specChar === true) {
+    compiled += symbols;
+    console.log("Symbols Result = " + compiled);
+    console.log(compiled.length);
+  }
+  else if (profile.upperCase === false && profile.lowerCase === false && profile.numeric === false) {
+    alert("At least one criteria must be selected. Please generate a new password.");
+  }
+
+  for (var i = 0; i < profile.length; i++) {
+    var randomChar = Math.floor(Math.random() * compiled.length);
+    randomized += compiled.charAt(randomChar);
+  }
+  console.log(randomized + " is " + randomized.length + " characters long");
+
+  return (randomized);
 }
 
