82a5eb385c5415dafc69f8c355b9ba6777b9c503
SageTengu <kyle.kleven@outlook.com>
2021-01-07 19:44:33 -0600

    Removed console logs used for testing, added comments, cleaned up readme

--- README.md
+++ README.md
@@ -10,30 +10,22 @@ Author: Kyle Kleven
 Course: UofM Coding Boot Camp 
 
 ### The Purpose
-This week’s homework requires you to create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+This week’s homework involves completing work on a password generator application that that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app runs in the browser, and features dynamically updated HTML and CSS powered by JavaScript code. It has a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of Password Special Characters from the OWASP Foundation](https://www.owasp.org/index.php/Password_special_characters).
-
-### The Refactor
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+### How it Works
+Click the button to generate a password. 
+You will be presented with a series of prompts for password criteria. 
+Select which criteria to include in the password. 
+Start by choose a length of at least 8 characters and no more than 128 characters.
+Next choose character types of lowercase, uppercase, numeric, and/or special characters
+Input is validated and at least one character must be selected
+When all prompts are answered, a password is generated that matches the selected criteria
+The password is then written to the page
 
 ### The Mock-up
 The following image shows the web application's appearance and functionality:
 
 ![password generator demo](./Assets/03-javascript-homework-demo.png)
+
 ## The Deliverable
-<live link>
+https://kdkleven.github.io/uofm-bootcamp-homework-03/
--- script/script.js
+++ script/script.js
@@ -4,87 +4,104 @@ var generateBtn = document.querySelector("#generate");
 
 // Declare variables containing all letters, numbers, and symbols
 var upperAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbers = [0,1,2,3,4,5,6,7,8,9];
-var symbols = ["!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]","\:",";","?",">","<",",",".","/","-","="];
-
-var userPassword = "";
+var lowerAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "`", "|", "}", "{", "[", "]", "\:", ";", "?", ">", "<", ",", ".", "/", "-", "="];
 
 // Declare an object variable containing the necessary criteria
 var profile = {
   length: 0,
-  upperCase: true,
-  lowerCase: true,
-  numeric: true,
-  specChar: true,
+  upperCase: false,
+  lowerCase: false,
+  numeric: false,
+  specChar: false,
+  rawPassword: "",
 
+  // function to prompt user for password length
   pwdLength: function () {
-    profile.length = parseInt(prompt("Choose your password length (must be a number between 8 and 128 characters"));
+    // prompt user for length and assign it to object's length property
+    this.length = parseInt(prompt("Choose your password length (must be a number between 8 and 128 characters"));
     
-    while (profile.length < 8 || profile.length > 128 || isNaN(profile.length) || profile.length == null)  {
-      profile.length = parseInt(prompt("Choose your password length (must be a number between 8 and 128 characters"));
+    // validate password length to meet requirements
+    while (this.length < 8 || this.length > 128 || isNaN(this.length)) {
+      this.length = parseInt(prompt("Choose your password length (must be a number between 8 and 128 characters"));
     }
-    console.log("length: " + this.length);  
+    
+    // return pasword length
+    return (this.length);
   },
 
+  // function to determine whether uppercase will be included or not
   pwdUpperCase: function () {
-    this.upperCase = confirm("Do you want to include upper case letters?");
-    console.log("upperCase: " + this.upperCase);
     
-    if (profile.upperCase === true) {
-      for (var u = 0; u < profile.length; u++) {
+    // prompt user for selection
+    this.upperCase = confirm("Do you want to include upper case letters?");
+    
+    // true, generate random upper case password that meets length requirements
+    if (this.upperCase === true) {
+      for (var u = 0; u < this.length; u++) {
         var upperRandom = Math.floor(Math.random() * upperAlpha.length);
-        userPassword += upperAlpha[upperRandom];
+        this.rawPassword += upperAlpha[upperRandom];
       }
-      console.log("upperPassword: " + userPassword + " length is " + userPassword.length + " and type is " + typeof(userPassword));
     }
-    return(userPassword);
+    
+    // return random password
+    return this.rawPassword;
   },
-
-  pwdLowerCase: function () {    
+  
+  // function to determine whether lowercase will be included or not
+  pwdLowerCase: function () {
+    
+    // prompt user for selection
     this.lowerCase = confirm("Do you want to include lower case letters?");
-    console.log("lowerCase: " + this.lowerCase);
-
-    if (profile.lowerCase === true) {
-      for (var l = 0; l < profile.length; l++) {
+    
+    // true, generate random lower case password that meets length requirements, appended to rawPassword
+    if (this.lowerCase === true) {
+      for (var l = 0; l < this.length; l++) {
         var lowerRandom = Math.floor(Math.random() * lowerAlpha.length);
-        userPassword += lowerAlpha[lowerRandom];  
+        this.rawPassword += lowerAlpha[lowerRandom];
       }
-    console.log("lowerPassword: " + userPassword + " length is " + userPassword.length + " and type is " + typeof(userPassword));
     }
-    return(userPassword);
+
+    // return random password
+    return this.rawPassword;
   },
 
-  pwdNumeric: function () {    
+  // function to determine whether lowercase will be included or not
+  pwdNumeric: function () {
+    
+    // prompt user for selection
     this.numeric = confirm("Do you want to include numbers?");
-    console.log("Numeric = " + this.numeric);
 
-    if (profile.numeric === true) {
-      for (var n = 0; n < profile.length; n++) {
+    // true, generate random numeric password that meets length requirements, appended to rawPassword
+    if (this.numeric === true) {
+      for (var n = 0; n < this.length; n++) {
         var numbersRandom = Math.floor(Math.random() * numbers.length);
-        userPassword += numbers[numbersRandom];  
+        this.rawPassword += numbers[numbersRandom];
       }
-    console.log("numbers: " + userPassword + " length is " + userPassword.length + " and type is " + typeof(userPassword));
     }
-    return(userPassword);
+
+    // return random password
+    return this.rawPassword;
   },
 
+  // function to determine whether symbols will be included or not
   pwdSpecChar: function () {   
     this.specChar = confirm("Do you want to include special characters?");
-    console.log("SpecChar = " + this.specChar);
     
-    if (profile.specChar === true) {
+    // true, generate random numeric password that meets length requirements, appended to rawPassword
+    if (this.specChar === true) {
       for (var s = 0; s < profile.length; s++) {
         var symbolsRandom = Math.floor(Math.random() * symbols.length);
-        userPassword += symbols[symbolsRandom];  
+        this.rawPassword += symbols[symbolsRandom];  
       }
-    console.log("symbols: " + userPassword + " length is " + userPassword.length + " and type is " + typeof(userPassword));
     }
-    else if (profile.upperCase === false && profile.lowerCase === false && profile.numeric === false) {
+    // validate to ensure at least one critera is selected (rerun the function)
+    else if (this.upperCase === false && this.lowerCase === false && this.numeric === false && this.specChar === false) {
       alert("At least one criteria must be selected. Please generate a new password.");
-      generatePassword();
+      return;
     }
-    return(userPassword);  
+    return(this.rawPassword);  
   }
 };
 
@@ -92,33 +109,35 @@ var profile = {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  //console.log("This is the password value: " + password + " and it is " + password.length + " characters");
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Generate the password
 function generatePassword() {
+  // ask user for various criteria
   profile.pwdLength();
   profile.pwdUpperCase();
   profile.pwdLowerCase();
   profile.pwdNumeric();
   profile.pwdSpecChar();
 
-  var combined = userPassword;
-  console.log("combined: " + combined);
-  var combinedLength = combined.length;
-  console.log("combinedLength: " + combinedLength);
+  // declare password length and result variables
+  var rawPasswordLength = profile.rawPassword.length;
   var result = "";
 
+  // iterate through the raw password and use math.random to generate a random password that meets the length of the user's input
   for (var i = 0; i < profile.length; i++) {
-    result += combined.charAt(Math.floor(Math.random() * combinedLength));
+    result += profile.rawPassword.charAt(Math.floor(Math.random() * rawPasswordLength));
   }
-  console.log(result);
-  
-  userPassword = "";
 
+  // reset raw password
+  profile.rawPassword = "";
+
+  // return generated password
   return result;
+  
 }
 
